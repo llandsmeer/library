916c81c35e1ebf1e11711ecadd11b59f5117f2c4
Lennart Landsmeer <lennart@landsmeer.email>
2024-10-16 15:10:55 +0200

    Simplify node design

--- library/nodes.py
+++ library/nodes.py
@@ -8,26 +8,59 @@ import models
 StateT = typing.TypeVar('StateT')
 ParamT = typing.TypeVar('ParamT')
 OutputT = typing.TypeVar('OutputT')
-ModelOutputT = typing.TypeVar('ModelOutputT')
 ContextT = typing.TypeVar('ContextT')
 InputT = typing.TypeVar('InputT')
-OuterInputT = typing.TypeVar('OuterInputT')
 
-class ABCBox(abc.ABC, typing.Generic[StateT, ParamT, OutputT, ContextT, ModelOutputT, InputT, OuterInputT]):
+OuterInputT = typing.TypeVar('OuterInputT')
+OuterOutputT = typing.TypeVar('OuterOutputT')
+
+class ABCBox(abc.ABC, typing.Generic[StateT, ParamT, OutputT, ContextT, InputT]):
     initial: StateT
     params: ParamT
     output: typing.Callable[[StateT, ParamT, ContextT], OutputT]
     step: typing.Callable[[StateT, ParamT, InputT], StateT]
-    input: typing.Callable[[OuterInputT, ModelOutputT], InputT]
-    context: typing.Callable[[OuterInputT], ContextT]
 
+@ABCBox.register
+class Box(typing.NamedTuple):
+    'Elementary Box'
+    initial: object
+    params: object
+    output: typing.Callable[[object, object, object], object]
+    step: typing.Callable[[object, object, object], object]
+
+class Connector(typing.Generic[OuterInputT, OuterOutputT, InputT, ContextT]):
+    'Goes with the Model class'
+    inner: ABCBox
+    input: typing.Callable[[OuterInputT, OuterInputT], InputT]
+    context: typing.Callable[[OuterInputT], ContextT]
+    def __init__(self, inner=None, *, input, context=None, initial=None, params=None):
+        def mkinputf(s: str) -> typing.Callable[[OuterInputT, OuterInputT], InputT]:
+            env = {}
+            exec(f'f = lambda input, output: {s}', env)
+            return env['f']
+        def mkcontextf(s: str) -> typing.Callable[[OuterInputT], ContextT]:
+            env = {}
+            exec(f'f = lambda input: {s}', env)
+            return env['f']
+        self.input = mkinputf(input) if isinstance(input, str) else input
+        self.context = (mkcontextf(context) if isinstance(context, str) else context) if context else lambda _:()
+        def f_output(state, params, ctx):
+            return state.output(params, *ctx) if isinstance(ctx, tuple) else state.output(params, ctx)
+        def f_step(state, params, inp):
+            return state.step(params, *inp) if isinstance(inp, tuple) else state.step(params, inp)
+        assert inner is None != (initial is None and params is None)
+        if inner is None:
+            self.inner = Box(initial, params, f_output, f_step) # type: ignore
+        else:
+            self.inner = inner
 
 class Model(ABCBox):
+    'Composite Box'
     def __init__(
             self,
             name: str,
             input: typing.List[str],
-            **kwargs: typing.Dict[str, ABCBox]
+            **kwargs: Connector
             ):
         members = kwargs.keys()
         self.State    = State   = collections.namedtuple(f'{name}State', members)
@@ -40,7 +73,7 @@ class Model(ABCBox):
                 s = getattr(state, k)
                 p = getattr(params, k)
                 c = member.context(inp)
-                o = member.output(s, p, c)
+                o = member.inner.output(s, p, c)
                 output.append(o)
             return Output(*output)
         def f_step(state: State, params: Params, inp: Input) -> State:
@@ -49,19 +82,18 @@ class Model(ABCBox):
             for k, member in kwargs.items():
                 s = getattr(state, k)
                 p = getattr(params, k)
-                c = member.context(inp)
                 i = member.input(inp, outer)
-                o = member.step(s, p, i)
-                state_next.append(s)
+                n = member.inner.step(s, p, i)
+                state_next.append(n)
             return State(*state_next)
         initial = []
         for member in kwargs.values():
-            i = member.initial
+            i = member.inner.initial
             initial.append(i)
         initial_state = State(*initial)
         params = []
         for member in kwargs.values():
-            i = member.params
+            i = member.inner.params
             params.append(i)
         params = Params(*params)
         output = f_output
@@ -71,42 +103,17 @@ class Model(ABCBox):
         self.output = output
         self.step = step
 
-
-class ABCBox(abc.ABC, typing.Generic[StateT, ParamT, OutputT, ContextT, ModelOutputT, InputT, OuterInputT]):
-    initial: StateT
-    params: ParamT
-    output: typing.Callable[[StateT, ParamT, ContextT], OutputT]
-    step: typing.Callable[[StateT, ParamT, InputT], StateT]
-    input: typing.Callable[[OuterInputT, ModelOutputT], InputT]
-    context: typing.Callable[[OuterInputT], ContextT]
-
-
-
-class GenericNode(ABCBox):
-    def __init__(self, initial, params, input, context=None):
-        self.initial = initial
-        self.params = params
-        self.input = input
-        self.context = context if context else lambda _:()
-        def f_output(state, params, ctx):
-            return state.output(params, *ctx) if isinstance(ctx, tuple) else state.output(params, ctx)
-        def f_step(state, params, inp):
-            return state.step(params, *inp) if isinstance(inp, tuple) else state.step(params, inp)
-        self.output = f_output
-        self.step = f_step
-
-
 model = Model('F',
         input = ['x'],
-        a = GenericNode(
+        a = Connector(
             initial=models.LIFState.make(10),
             params = models.LIParams.make(),
             input = lambda input, output: output.b + input.x # type: ignore
             ),
-        b = GenericNode(
+        b = Connector(
             initial=models.LIFState.make(10),
-            params = models.LIParams.make(),
-            input = lambda _, output: output.a # type: ignore
+            params =models.LIParams.make(),
+            input  = 'output.a' # type: ignore
             )
         )
 
